7155d880443be2fecde4fb9035f650a1cc521957
darin dieckhoff <darin dieckhoff>
2017-01-19 23:08:00 -0600

    added eslint and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,73 @@
 'use strict';
 
-    var gulp = require('gulp'),
-      concat = require('gulp-concat'),
-      uglify = require('gulp-uglify'),
-      rename = require('gulp-rename'),
-  sourcemaps = require('gulp-sourcemaps'),
-        sass = require('gulp-sass'),
-        csso = require('gulp-csso'),
-    imagemin = require('gulp-imagemin'),
-         del = require('del');
+  var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+sourcemaps = require('gulp-sourcemaps'),
+      sass = require('gulp-sass'),
+      csso = require('gulp-csso'),
+  imagemin = require('gulp-imagemin'),
+       del = require('del'),
+    eslint = require('gulp-eslint'),
+    useref = require('gulp-useref');
 
-gulp.task('concatscripts', function() {
+//---lints .js files and stops build on error
+gulp.task('eslint', function () {
+  return gulp.src(['/**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+//---concatinates, minifies, creates source maps for .js files
+gulp.task('scripts', ['eslint'], function() {
   return gulp.src('js/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('all.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./js'));
-});
-
-gulp.task('scripts', ['concatscripts'], function (){
-  return gulp.src('js/all.js')
-    .pipe(uglify())
     .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('./dist/scripts'));
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('compilesass', function () {
+//---compiles, minifies, creates source maps for sass files
+gulp.task('styles', function () {
   return gulp.src('sass/global.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('sass'));
-});
-
-gulp.task('styles', ['compilesass'], function (){
-  return gulp.src('css/all.css')
-    .pipe(csso())
+    .pipe(sass().on('error', sass.logError))
     .pipe(rename('all.min.css'))
+    .pipe(csso())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist/styles'));
 });
 
+//---optimizes images
 gulp.task('images', function () {
   gulp.src('images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/content'));
 });
 
+//---watches global.scss and javascript files for changes and runs tasks
+gulp.task('watch', function () {
+  gulp.watch('sass/global.scss', ['styles']);
+  gulp.watch('js/**/*.js', ['scripts']);
+});
+
+//---clears out dist folder
 gulp.task('clean', function () {
   del(['dist/*']);
 });
 
-gulp.task('build', ['scripts', 'images'], function () {
+//---build task that runs scripts, styles, and images tasks. Moves icons folder to dist directory
+gulp.task('build', ['scripts', 'styles', 'images'], function () {
   gulp.src('icons/*')
   .pipe(gulp.dest('dist/icons'));
 });
 
+
+//---sets build as default gulp task and runs clean task prior to build
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
